52b589d4d14528e43113704878c514a68d4e315f
John Jarman <johncjarman@gmail.com>
2020-04-30 15:06:32 +0100

    Add IP display on start-up

--- displayprice.py
+++ displayprice.py
@@ -3,7 +3,24 @@ import time
 import datetime
 import board
 import busio
+import socket
 from adafruit_ht16k33 import segments
+
+def get_ip():
+    """
+    Utility function to find the IP address of the Pi.
+    From https://stackoverflow.com/a/28950776
+    """
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.255.255.255', 1))
+        IP = s.getsockname()[0]
+    except:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
 
 # Create the I2C interface.
 i2c = busio.I2C(board.SCL, board.SDA)
@@ -15,6 +32,13 @@ display = segments.Seg7x4(i2c)
 # Display test
 display.fill(1)
 time.sleep(1)
+display.fill(0)
+
+# Display IP address
+host_ip = get_ip()
+for i in range(0, 2):
+    display.marquee(host_ip + '    ', 0.5, False)
+
 display.fill(0)
 
 api_key = octopusenergy.load_api_key_from_file('api_key.txt')
